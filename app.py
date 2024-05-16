dd7c8eca0dc69a7495f8786fc433104c9d735cbc
SHIVAJEE <130691874+shivajee98@users.noreply.github.com>
2024-05-16 23:51:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,49 +4,50 @@ import time
 
 app = Flask(__name__)
 
-# Configure the serial port
-serial_port = '/dev/ttyACM1'  # Update with the correct serial port of your Arduino
+# Configure the possible serial ports
+serial_ports = ['/dev/ttyACM0', '/dev/ttyACM1']  # Add more if needed
 baud_rate = 115200
-
 
 # Function to establish serial connection with Arduino
 def establish_serial_connection():
-    while True:
+    for port in serial_ports:
         try:
-            ser = serial.Serial(serial_port, baud_rate, timeout=1)
+            ser = serial.Serial(port, baud_rate, timeout=1)
             if ser.is_open:
-                print("Serial connection established with Arduino")
+                print(f"Serial connection established with Arduino on {port}")
                 return ser
         except serial.SerialException:
-            print(f"Failed to connect to {serial_port}. Retrying in 5 seconds...")
-            time.sleep(5)
+            print(f"Failed to connect to {port}. Trying next port...")
 
+    # If no port is successfully connected
+    print("Failed to connect to any serial port.")
+    return None
 
 # Establish serial connection
 ser = establish_serial_connection()
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/send_command/<direction>')
 def send_command(direction):
     if direction in ['F', 'B', 'L', 'R', 'A', '1', '2', '3', '4', 'S']:
-        ser.write(direction.encode())
-        return f'Sent command: {direction}'
+        if ser:
+            ser.write(direction.encode())
+            return f'Sent command: {direction}'
+        else:
+            return 'Failed to send command. Arduino not connected.'
     else:
         return 'Invalid action'
-
 
 @app.route('/start_radar')
 def start_radar():
     # Add code here to run the radar functionality
     return 'Radar started successfully'
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-
-
+    if ser:
+        app.run(host='0.0.0.0', port=80)
+    else:
+        print("Exiting Flask application. Arduino not connected.")
